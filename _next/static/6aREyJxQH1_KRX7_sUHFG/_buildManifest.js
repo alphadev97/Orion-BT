83778bdcda8fc65948e1f17e4f6c4ade52f0088f
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-11-13 21:38:45 +0500

    Orion

--- _next/static/6aREyJxQH1_KRX7_sUHFG/_buildManifest.js
+++ _next/static/6aREyJxQH1_KRX7_sUHFG/_buildManifest.js
@@ -1,1 +1,69 @@
-self.__BUILD_MANIFEST=function(s,c){return{__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/ea88be26-b21151d5035ec7c2.js",s,"static/chunks/652-439cf60d7e9b2ee8.js",c,"static/chunks/pages/index-346a10ff43d279c9.js"],"/404":[s,"static/chunks/pages/404-29adefd5c7f6f170.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],"/contact":[s,"static/chunks/154-c97fe60170412384.js","static/chunks/pages/contact-b112bf4500db7f5c.js"],"/culture":[s,c,"static/chunks/pages/culture-42e23e3f89204d72.js"],"/disclosures":[s,"static/chunks/pages/disclosures-73d948525f79770e.js"],"/disclosures/cookies-and-privacy-policy":[s,"static/chunks/pages/disclosures/cookies-and-privacy-policy-48ae3a8ff03f4f1d.js"],"/disclosures/disclaimer":[s,"static/chunks/pages/disclosures/disclaimer-a15ac5ba5c1a0990.js"],"/disclosures/eu-sustainability-related-disclosures":[s,"static/chunks/pages/disclosures/eu-sustainability-related-disclosures-7ab4d9b1dcb58905.js"],"/disclosures/eu-sustainability-related-disclosures/orion":[s,"static/chunks/pages/disclosures/eu-sustainability-related-disclosures/orion-cb66a8deea7da503.js"],"/disclosures/human-rights-policy":[s,"static/chunks/pages/disclosures/human-rights-policy-8e017e7856f04c9b.js"],"/disclosures/responsible-practices-and-investment-policy":[s,"static/chunks/pages/disclosures/responsible-practices-and-investment-policy-a329551aa35c56dd.js"],"/investment-solutions":[s,"static/chunks/pages/investment-solutions-2ea2a34a4afd5768.js"],sortedPages:["/","/404","/_app","/_error","/contact","/culture","/disclosures","/disclosures/cookies-and-privacy-policy","/disclosures/disclaimer","/disclosures/eu-sustainability-related-disclosures","/disclosures/eu-sustainability-related-disclosures/orion","/disclosures/human-rights-policy","/disclosures/responsible-practices-and-investment-policy","/investment-solutions"]}}("static/chunks/23-253e78db147f2682.js","static/chunks/348-a0501479b9f2eef5.js"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
+(self.__BUILD_MANIFEST = (function (s, c) {
+  return {
+    __rewrites: { afterFiles: [], beforeFiles: [], fallback: [] },
+    "/": [
+      "static/chunks/ea88be26-b21151d5035ec7c2.js",
+      s,
+      "static/chunks/652-439cf60d7e9b2ee8.js",
+      c,
+      "static/chunks/pages/index-346a10ff43d279c9.js",
+    ],
+    "/404": [s, "static/chunks/pages/404-29adefd5c7f6f170.js"],
+    "/_error": ["static/chunks/pages/_error-54de1933a164a1ff.js"],
+    "/contact": [
+      s,
+      "static/chunks/154-c97fe60170412384.js",
+      "static/chunks/pages/contact-b112bf4500db7f5c.js",
+    ],
+    "/culture": [s, c, "static/chunks/pages/culture-42e23e3f89204d72.js"],
+    "/disclosures": [s, "static/chunks/pages/disclosures-73d948525f79770e.js"],
+    "/disclosures/cookies-and-privacy-policy": [
+      s,
+      "static/chunks/pages/disclosures/cookies-and-privacy-policy-48ae3a8ff03f4f1d.js",
+    ],
+    "/disclosures/disclaimer": [
+      s,
+      "static/chunks/pages/disclosures/disclaimer-a15ac5ba5c1a0990.js",
+    ],
+    "/disclosures/eu-sustainability-related-disclosures": [
+      s,
+      "static/chunks/pages/disclosures/eu-sustainability-related-disclosures-7ab4d9b1dcb58905.js",
+    ],
+    "/disclosures/eu-sustainability-related-disclosures/orion": [
+      s,
+      "static/chunks/pages/disclosures/eu-sustainability-related-disclosures/orion-cb66a8deea7da503.js",
+    ],
+    "/disclosures/human-rights-policy": [
+      s,
+      "static/chunks/pages/disclosures/human-rights-policy-8e017e7856f04c9b.js",
+    ],
+    "/disclosures/responsible-practices-and-investment-policy": [
+      s,
+      "static/chunks/pages/disclosures/responsible-practices-and-investment-policy-a329551aa35c56dd.js",
+    ],
+    "/investment-solutions": [
+      s,
+      "static/chunks/pages/investment-solutions-2ea2a34a4afd5768.js",
+    ],
+    sortedPages: [
+      "/",
+      "/404",
+      "/_app",
+      "/_error",
+      "/contact",
+      "/culture",
+      "/disclosures",
+      "/disclosures/cookies-and-privacy-policy",
+      "/disclosures/disclaimer",
+      "/disclosures/eu-sustainability-related-disclosures",
+      "/disclosures/eu-sustainability-related-disclosures/orion",
+      "/disclosures/human-rights-policy",
+      "/disclosures/responsible-practices-and-investment-policy",
+      "/investment-solutions",
+    ],
+  };
+})(
+  "static/chunks/23-253e78db147f2682.js",
+  "static/chunks/348-a0501479b9f2eef5.js"
+)),
+  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
